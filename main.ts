f09b5a5bc122aed0047d248b6382017fc6dfc2c1
MrsBrownDeshler <68723104+MrsBrownDeshler@users.noreply.github.com>
2021-10-30 11:58:08 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C C C D E - - - ", 120)
+    music.playMelody("C C C D D C C C ", 120)
+    music.playMelody("C C C D E - - - ", 120)
+    music.playMelody("C C C D E D C C ", 120)
+    music.playMelody("C D C C5 B C D - ", 120)
+    music.playMelody("C D C G C C D - ", 120)
+    music.playMelody("C D C C5 B C D - ", 120)
+    music.playMelody("C D C G C C G C ", 120)
+    music.playMelody("D - C C D E E - ", 120)
+})
+basic.forever(function () {
+	
+})
